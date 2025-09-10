085b5b26bf089570f67eaa7bc2f96886c0f576f4
albertomelegatti <130663766+albertomelegatti@users.noreply.github.com>
2025-09-10 14:30:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import threading
+import time
+import requests
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -5,6 +8,18 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+
+# Funzione che fa ping ogni 10 minuti
+def keep_alive():
+    while True:
+        try:
+            requests.get("https://webapp-fantamanageriale.onrender.com/")
+        except:
+            pass
+        time.sleep(840)  # 600 secondi = 10 minuti
+
+# Avvia il thread quando si lancia il server
+threading.Thread(target=keep_alive, daemon=True).start()
 
 if __name__ == "__main__":
     app.run(debug=True)
