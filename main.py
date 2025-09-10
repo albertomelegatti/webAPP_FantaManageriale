533f7568301196fe9a657dff2041bbf5d6feb619
albertomelegatti <130663766+albertomelegatti@users.noreply.github.com>
2025-09-10 14:44:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,5 +6,12 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/scarica-regolamento")
+def scarica_regolamento():
+    directory = os.path.join(app.root_path, "static")
+    filename = "Regolamento_Fantacalcio_Manageriale.pdf"
+    return send_from_directory(directory, filename, as_attachment=True)
+
 if __name__ == "__main__":
     app.run(debug=True)
+
