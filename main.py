50b48e82c474a2dfbb90be2da8950ff41a534cf3
albertomelegatti <130663766+albertomelegatti@users.noreply.github.com>
2025-09-12 11:48:13 +0200

    Add files via upload
    Alcuni cambiamenti

--- main.py
+++ main.py
@@ -13,7 +13,27 @@ def scarica_regolamento():
     filename = "regolamento.pdf"
     return send_from_directory(directory, filename, as_attachment=True)
 
+# Rotta per login admin
+@app.route("/login-admin")
+def login_admin():
+    return render_template("adminLogin.html")
+
+# Rotta per login squadre
+@app.route("/login-squadre")
+def login_squadre():
+    return render_template("squadre.html")
+
+@app.route("/rose")
+def rose():
+    return "<h2>Sezione Rose (in costruzione)</h2>"
+
+@app.route("/stadi")
+def stadi():
+    return "<h2>Sezione Stadi (in costruzione)</h2>"
+
+@app.route("/prestiti")
+def prestiti():
+    return "<h2>Sezione Prestiti (in costruzione)</h2>"
+
 if __name__ == "__main__":
     app.run(debug=True)
-
-
