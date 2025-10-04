c1f8b3083222a98f2560690bb9aab48a2ee45fcc
albertomelegatti <melegattialberto@gmail.com>
2025-10-04 17:29:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -381,7 +381,5 @@ def ask():
 
 
 if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
 
