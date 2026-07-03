d03037c0db65dafa90716145a5efa653c6843f6e
andreamurari <murariandrea00@gmail.com>
2026-07-03 02:21:08 +0200

    Revert "Modificato il menu a tendina nella schermata di modifica del contratto"
    This reverts commit ccfa1a3e714c466bd7b03740206d011dbfc8d1b3.

--- CronJob/cron_job_prestiti.sql
+++ CronJob/cron_job_prestiti.sql
@@ -1,4 +1,44 @@
--- 3.1 Tipo prestito = OBBLIGO DI RISCATTO
+BEGIN
+  -- 1. Tipo prestito = SECCO
+  -- Riporta il giocatore alla squadra di origine e imposta il contratto a Indeterminato
+  UPDATE giocatore g
+  SET 
+      squadra_att = p.squadra_prestante,
+      tipo_contratto = 'Indeterminato'
+  FROM prestito p
+  WHERE g.id = p.giocatore
+    AND p.stato = 'in_corso'
+    AND p.tipo_prestito = 'secco'
+    AND p.data_fine <= NOW() AT TIME ZONE 'Europe/Rome';
+
+  UPDATE prestito
+  SET stato = 'terminato'
+  WHERE stato = 'in_corso'
+    AND tipo_prestito = 'secco'
+    AND data_fine <= NOW() AT TIME ZONE 'Europe/Rome';
+
+
+  -- 2. Tipo prestito = DIRITTO DI RISCATTO
+  -- Gestione dei prestiti con diritto di riscatto che non vengono riscattati
+  UPDATE giocatore g
+  SET
+      squadra_att = p.squadra_prestante,
+      tipo_contratto = 'Indeterminato'
+  FROM prestito p
+  WHERE p.giocatore = g.id
+    AND p.tipo_prestito = 'diritto_di_riscatto'
+    AND p.stato = 'in_corso'
+    AND p.data_fine <= NOW() AT TIME ZONE 'Europe/Rome';
+
+  UPDATE prestito
+  SET stato = 'terminato'
+  WHERE tipo_prestito = 'diritto_di_riscatto'
+    AND stato = 'in_corso'
+    AND data_fine <= NOW() AT TIME ZONE 'Europe/Rome';
+
+
+
+  -- 3.1 Tipo prestito = OBBLIGO DI RISCATTO
   UPDATE giocatore g
   SET
       squadra_att = p.squadra_ricevente,
@@ -18,18 +58,18 @@
   -- 3.2 Aggiornamento crediti per obbligo di riscatto
   UPDATE squadra s
   SET
-      crediti = s.crediti - p.crediti_riscatto
+      crediti = s.crediti - p.costo_riscatto
   FROM prestito p
-  WHERE p.squadra_ricevente = s.nome
+  WHERE p.squadra_ricevente = s.nome_squadra
     AND p.tipo_prestito = 'obbligo_di_riscatto'
     AND p.stato = 'riscattato'
     AND p.data_fine <= NOW() AT TIME ZONE 'Europe/Rome';
 
   UPDATE squadra s
   SET
-      crediti = s.crediti + p.crediti_riscatto
+      crediti = s.crediti + p.costo_riscatto
   FROM prestito p
-  WHERE p.squadra_prestante = s.nome
+  WHERE p.squadra_prestante = s.nome_squadra
     AND p.tipo_prestito = 'obbligo_di_riscatto'
     AND p.stato = 'riscattato'
     AND p.data_fine <= NOW() AT TIME ZONE 'Europe/Rome';
@@ -42,8 +82,11 @@
     AND stato = 'riscattato'
     AND data_fine <= NOW() AT TIME ZONE 'Europe/Rome';
 
-  UPDATE giocatore g
+# Gestione Prestiti Reali
+  UPDATE giocatore
   SET 
       squadra_att = detentore_cartellino,
       tipo_contratto = 'Indeterminato'
   WHERE g.tipo_contratto = 'Prestito Reale'
+
+END;
